01c03f07194b3769736bac58186c9baa8e28b0a4
KavinPragadeesh <kavinswealthvalley@gmail.com>
2021-08-24 15:27:57 +0530

    jenkin sample hit

--- JenkinsDemo/src/main/java/com/example/demo/JenkinsDemoApplication.java
+++ JenkinsDemo/src/main/java/com/example/demo/JenkinsDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Jenkins !!");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
